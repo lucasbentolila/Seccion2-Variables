f331011e197e1623fc663e8ee6635d6af43fdbb6
Lucas Bentolila <lucasbentolila17@gmail.com>
2025-11-13 02:12:26 -0300

    Primer ejercicio del curso: Detalle de Factura. Agrego comentarios

--- src/EjercicioDetalleDeFactura.java
+++ src/EjercicioDetalleDeFactura.java
@@ -15,14 +15,18 @@ import java.util.Scanner;
 public class EjercicioDetalleDeFactura {
     public static void main(String[] args) {
 
+        // Creamos el objeto Scanner para leer datos del usuario
         Scanner scanner = new Scanner(System.in);
 
+        // Pedimos el nombre o descripción de la factura
         System.out.println("Hola bienvenido, ingrese el nombre o descripción de la factura: ");
         String nombreFactura = scanner.nextLine();
 
+        // Declaramos las variables donde guardamos los precios
         double precio1 = 0;
         double precio2 = 0;
 
+        // Bloque try-catch para controlar si el usuario ingresa datos no númericos
         try {
             System.out.println("Ingrese el precio del primer producto");
             precio1 = scanner.nextDouble();
@@ -31,18 +35,28 @@ public class EjercicioDetalleDeFactura {
             precio2 = scanner.nextDouble();
 
         } catch (InputMismatchException e) {
+            // Si el usuario ingresa algo que no sea número, mostramos mensaje de error
             System.out.println("Dato ingresado no valido, por favor ingrese datos nuevamente");
-            return;
+            return; // terminamos el programa para evitar errores
         }
 
+        // Calculamos el total bruto (suma de los dos precios)
         double totalBruto = precio1 + precio2;
+
+        // Calculamos el impuesto del 19%
         double impuesto = totalBruto * 0.19;
+
+        // Calculamos el total neto (suma del total bruto + impuesto)
         double totalNeto = totalBruto + impuesto;
 
+        // Creamos un String para mostrar toda la información de forma clara, con catenaciones
         String notificacion = "La factura " + nombreFactura + " tiene un total bruto de " + totalBruto + ", con un impuesto de " + impuesto +
                 " y el monto despues del impuesto es de " + totalNeto;
 
+        //Mostramos el resultado final
         System.out.println(notificacion);
+
+        scanner.close(); // Cerramos el scanner para liberar recursos
 
     }
 }
